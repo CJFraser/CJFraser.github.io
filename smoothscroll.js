6911954bfa2dd4bc4ee01a24dd7268f0c307ef3e
CJFraser <cfraser1037@gmail.com>
2015-08-24 15:16:38 -0400

    Update smoothscroll.js

--- smoothscroll.js
+++ smoothscroll.js
@@ -1,1 +1,69 @@
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 
+
+
+$(window).scroll(function(){
+	var wScroll = $(this).scrollTop();
+	
+	$('.backgroundimgfull').css({
+		'transform' : 'translate(0px,'+ (wScroll)/4 +'px)'
+	});
+   
+ });
+ 
+ 
+ $(window).scroll(function(){
+	var wScroll = $(this).scrollTop();
+	
+	if(wScroll > $('.breakertext2').offset().top -$(window).height()) {
+	
+	$('.breakertext2').css({
+		'opacity':1 - wScroll/200
+	});
+	};
+	
+ });
+ 
+
+$(window).scroll(function(){
+	var wScroll = $(this).scrollTop(); 
+	
+	if(wScroll > $('.textarea').offset().top -$(window).height()) {
+	
+	$('.textarea').css({
+		'transform' : 'translate(0px,'+ (wScroll)/5 +'px)'
+	});
+	};
+   
+ });
+  
+
+$(document).ready(function() {
+	
+		var fade = $(".breakertext2").offset().top;
+		
+		$(window).scroll(function() {
+			var scrollPos = $(window).scrollTop();
+			
+			  if(scrollPos >= fade) {
+				$(".mediaicons").addClass("mediafade");
+			} 
+			 else{
+				$(".mediaicons").removeClass("mediafade");
+			} 
+		});
+	
+});
